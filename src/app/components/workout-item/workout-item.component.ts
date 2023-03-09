f8b8fcd6af7e36852f16224a34d4e6253ee219fc
Radicitus <novivolatus@gmail.com>
2023-03-09 12:19:05 -0800

    Correct spacing for workout items in workout item list

--- src/app/components/workout-item/workout-item.component.ts
+++ src/app/components/workout-item/workout-item.component.ts
@@ -7,19 +7,19 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class WorkoutItemComponent implements OnInit {
 
-  @Input() bodyGroup:string;
-  @Input() workoutName:string;
-  @Input() workoutWeight:number;
-  @Input() workoutReps:number;
+  @Input() bodyGroup:string = "";
+  @Input() workoutName:string = "";
+  @Input() workoutWeight:number = 0;
+  @Input() workoutReps:number = 0;
   @Input() workoutInstructions?:string;
 
 
   constructor() {
-    this.bodyGroup = "arms";
-    this.workoutName = "bicep curl";
-    this.workoutWeight = 25;
-    this.workoutReps = 10;
-    this.workoutInstructions = "Start with carrying the weight above your head. Then, push it up. Next, eat a donut to give yourself energy..."
+    // this.bodyGroup = "arms";
+    // this.workoutName = "bicep curl";
+    // this.workoutWeight = 25;
+    // this.workoutReps = 10;
+    // this.workoutInstructions = "Start with carrying the weight above your head. Then, push it up. Next, eat a donut to give yourself energy..."
   }
 
   ngOnInit() {
