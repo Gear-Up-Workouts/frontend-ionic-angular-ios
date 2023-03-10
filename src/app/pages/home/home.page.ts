dd7b53c014586449146899ac9e70bb2d429d2488
Radicitus <novivolatus@gmail.com>
2023-03-09 17:24:30 -0800

    Checkpoint

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,35 +1,59 @@
 import { Component } from '@angular/core';
-import {interval} from "rxjs";
-import {WorkoutData} from "../../data/workout-data";
+import { interval } from 'rxjs';
+import { WorkoutData } from '../../data/workout-data';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
   date: Date;
   day: string = '';
-  dailyMessage:string;
+  dailyMessage: string = 'Rise amd Grind!';
 
-  workoutItems:WorkoutData[];
+  workoutItems: WorkoutData[];
 
   constructor() {
-
     // Fake data
     this.workoutItems = [
-      new WorkoutData({bodyGroup: "arms", workoutName: "bicep curls", workoutWeight: 25, workoutReps: 10}),
-      new WorkoutData({bodyGroup: "back", workoutName: "lat pull down", workoutWeight: 65, workoutReps: 8}),
-      new WorkoutData({bodyGroup: "legs", workoutName: "squat", workoutWeight: 125, workoutReps: 4}),
-      new WorkoutData({bodyGroup: "arms", workoutName: "tri pull downs", workoutWeight: 35, workoutReps: 10}),
-      new WorkoutData({bodyGroup: "back", workoutName: "rows", workoutWeight: 55, workoutReps: 7}),
+      new WorkoutData({
+        bodyGroup: 'arms',
+        workoutName: 'bicep curls',
+        workoutWeight: 25,
+        workoutReps: 10,
+      }),
+      new WorkoutData({
+        bodyGroup: 'back',
+        workoutName: 'lat pull down',
+        workoutWeight: 65,
+        workoutReps: 8,
+      }),
+      new WorkoutData({
+        bodyGroup: 'legs',
+        workoutName: 'squat',
+        workoutWeight: 125,
+        workoutReps: 4,
+      }),
+      new WorkoutData({
+        bodyGroup: 'arms',
+        workoutName: 'tri pull downs',
+        workoutWeight: 35,
+        workoutReps: 10,
+      }),
+      new WorkoutData({
+        bodyGroup: 'back',
+        workoutName: 'rows',
+        workoutWeight: 55,
+        workoutReps: 7,
+      }),
     ];
 
     this.date = new Date();
     this.updateDay();
     this.setAutoUpdateDate();
 
-    this.dailyMessage = "Pump some iron!";
+    this.dailyMessage = 'Pump some iron!';
   }
 
   updateDay() {
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -31,4 +31,8 @@ export class ApiService {
   //   });
   // }
 
+  test() {
+    return this.sendRequestToApi("/");
+  }
+
 }
