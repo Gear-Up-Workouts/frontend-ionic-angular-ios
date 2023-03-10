3641762ebe1579c1f21f8640d21bce3fd609cd83
Radicitus <novivolatus@gmail.com>
2023-03-09 18:26:10 -0800

    Ignore error

--- src/app/components/workout-item-list/workout-item-list.component.ts
+++ src/app/components/workout-item-list/workout-item-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { WorkoutData } from '../../data/workout-data';
 
+// noinspection AngularMissingOrInvalidDeclarationInModule
 @Component({
   selector: 'app-workout-item-list',
   templateUrl: './workout-item-list.component.html',
--- src/app/components/workout-item/workout-item.component.ts
+++ src/app/components/workout-item/workout-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { WorkoutData } from '../../data/workout-data';
 
+// noinspection AngularMissingOrInvalidDeclarationInModule
 @Component({
   selector: 'app-workout-item',
   templateUrl: './workout-item.component.html',
